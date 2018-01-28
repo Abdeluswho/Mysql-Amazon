171321566439f8511ae84d705306de334ee575b1
A.Sofiane Guerfi <asofianeguerfi@ASofianes-MacBook-Pro.local>
2018-01-28 15:11:22 -0800

    Customer-Version

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,14 +20,14 @@ connection.connect(function (err) {
 	// body...
 	if (err) throw err;
 	console.log('connect as ID: ', connection.threadId);
-	SELECTALL();
+	displayInventory();
 
 	setTimeout(promptUser, 2000);
 });
 
 
 //Select all the products to display for the customer
-function SELECTALL() {
+function displayInventory() {
 	// body...
 	connection.query("SELECT * from products ", function(err,res){
 		if (err) throw err;
@@ -83,9 +83,55 @@ function promptUser() {
 			let id = input.productID;
 			let quantity = input.quantity;
 
-	    		console.log("ID ", id + "Quantity ", quantity);
-	});
+	      
+	               // Query db to confirm that the given item ID exists in the desired quantity
+        var queryInv = 'SELECT * FROM products WHERE ?';
 
+        connection.query(queryInv, {item_id: id}, function(err, data) {
+            if (err) throw err;
+
+            // If the user has selected an invalid item ID, data array will be empty
+
+            if (data.length === 0) {
+                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                displayInventory();
+
+            } else {
+                var productData = data[0];
+
+                // console.log('productData = ' + JSON.stringify(productData));
+                // console.log('productData.stock_quantity = ' + productData.stock_quantity);
+
+                // If the quantity requested by the user is in stock
+                if (quantity <= productData.stock_quantity) {
+                    console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+                    // Construct the updating query string
+                    var updateQueryInv = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + id;
+                    // console.log('updateQueryStr = ' + updateQueryStr);
+
+                    // Update the inventory
+                    connection.query(updateQueryInv, function(err, data) {
+                        if (err) throw err;
+
+                        console.log('Your order has been placed! Your total is $' + productData.price_usd * quantity);
+                        console.log('Thank you for shopping with us!');
+                        console.log("\n---------------------------------------------------------------------\n");
+
+                        // End the database connection
+                        connection.end();
+                    })
+                } else {
+                    console.log('Sorry, there is not enough product in stock, your order can not be placed as it is.');
+                    console.log('Please modify your order.');
+                    console.log("\n---------------------------------------------------------------------\n");
+
+                    promptUser();
+                }
+            }
+        
+		});
+     })
 }
 
 
